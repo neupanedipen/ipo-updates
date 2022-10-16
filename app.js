2343343a9ad2fa92cf6e831638370fb1661c7326
Dipendra Neupane <deardipen50@gmail.com>
2022-10-16 11:15:44 +0545

    added issues closing today list

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const openIposElement = document.querySelector(".openIPO-cards");
 const emptyIpo = document.querySelector(".empty-ipo");
 
+const closingTodayEle = document.querySelector("#closingToday")
+
 const upcomingIposElement = document.querySelector(".upcomingIPO-cards");
 const emptyUpcoming = document.querySelector(".empty-upcoming");
 
@@ -21,6 +23,46 @@ const fetchData = async () => {
 };
 
 const checkOpen = (arr) => {
+  const closingToday = arr.filter(ipo => {
+    return new Date (ipo.closeDate).toLocaleDateString() == today.toLocaleDateString()
+  })
+
+  if(closingToday.length > 0){
+
+    let closingTitle = document.createElement("h2");
+    closingTitle.innerHTML = 'Issues Closing Today';
+    closingTodayEle.appendChild(closingTitle)
+
+    closingToday.map((ipo) => {
+      let closingToday = document.createElement("div");
+      closingToday.classList.add('closingToday');
+
+      let name = document.createElement("h3");
+      name.innerHTML = `${ipo.company} (${ipo.ticker})`;
+      closingToday.appendChild(name);
+
+      let description = document.createElement("p");
+      description.innerHTML = `<strong>Description: </strong> ${ipo.details}`;
+      closingToday.appendChild(description);
+
+      let openDate = document.createElement("p");
+      openDate.innerHTML = `<strong>Open Date: </strong> ${new Date(
+        ipo.openDate
+      ).toDateString()}`;
+      closingToday.appendChild(openDate);
+      document.body.appendChild(closingToday);
+
+      let closeDate = document.createElement("p");
+      closeDate.innerHTML = `<strong>Close Date:</strong> ${new Date(
+        ipo.closeDate
+      ).toDateString()}`;
+      closingToday.appendChild(closeDate);
+
+      closingTodayEle.appendChild(closingToday)
+      
+    })    
+  }
+
   const upcomingIPOs = arr.filter((item) => {
     let openDate = new Date(item.openDate);
     let closeDate = new Date(item.closeDate);
@@ -63,7 +105,7 @@ const checkOpen = (arr) => {
   const openIPOs = arr.filter((item) => {
     let openDate = new Date(item.openDate);
     let closeDate = new Date(item.closeDate);
-    return openDate <= today && closeDate > today;
+    return openDate <= today && closeDate >= today;
   });
   if (openIPOs.length < 1) {
     openIposElement.style.display = "none";
